62c7131ca8bcc73b23cc7e4c6c71fd8190e9f69f
Josue Carrera <josuecarrera788@gmail.com>
2025-11-11 23:36:14 -0300

    Proyecto TTPS2025 con appmascotas integrado

---
+++ appmascotas/src/main/java/ttps/grupo2/appmascotas/AppmascotasApplication.java
@@ -0,0 +1,13 @@
+package ttps.grupo2.appmascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmascotasApplication.class, args);
+	}
+
+}
---
+++ appmascotas/src/test/java/ttps/grupo2/appmascotas/AppmascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package ttps.grupo2.appmascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
