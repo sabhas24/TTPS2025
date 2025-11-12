938019957d7d8538a30d2a7878aad8b0d8080c4a
Patricio Benjamin Serres <patricioserres@gmail.com>
2025-11-12 02:26:15 -0300

    Nuevo backend Spring Boot desde cero

---
+++ src/test/java/ttps/grupo2/appmascotas/AppMascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package ttps.grupo2.appmascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMascotasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
