3c27bf98362f7fe6f6c1d4cf9646f67a08a4916e
pablo redruello <pablo.redruello246513@alumnos.info.unlp.edu.ar>
2025-12-13 16:55:37 -0300

    correcion de un error en el login

--- frontend/src/app/services/auth.ts
+++ frontend/src/app/services/auth.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
-import { LoginRequest, JwtResponse } from '../interfaces/auth';
+import { UsuarioLogin, JwtResponse } from '../interfaces/usuario';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
-const API_BASE_URL = 'http://localhost:8080'; // ajusta si es otro puerto
-
+const API_BASE_URL = 'http://localhost:8080';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,9 +12,9 @@ export class AuthService {
 
   private readonly TOKEN_KEY = 'auth_token';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  login(credentials: LoginRequest): Observable<JwtResponse> {
+  login(credentials: UsuarioLogin): Observable<JwtResponse> {
     return this.http.post<JwtResponse>(`${API_BASE_URL}/auth/login`, credentials).pipe(
       tap(response => {
         this.setToken(response.token);
