87ec157c10ea4049911c671a03627c94e48e778e
pablo redruello <pablo.redruello246513@alumnos.info.unlp.edu.ar>
2025-12-13 18:34:28 -0300

    registro de prueba

--- frontend/src/app/component/login/login.ts
+++ frontend/src/app/component/login/login.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth'
+import { UsuarioLogin } from '../../interfaces/usuario';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+
+  email = '';
+  password = '';
+  error = '';
+
+  constructor(private authService: AuthService) { }
+
+  onSubmit() {
+    const body: UsuarioLogin = { email: this.email, contraseña: this.password };
+
+    this.authService.login(body).subscribe({
+      next: () => {
+
+      },
+      error: err => {
+        this.error = 'Credenciales inválidas';
+        console.error(err);
+      }
+    });
+  }
 
 }
