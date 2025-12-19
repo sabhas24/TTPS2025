fe30b8e80dff49fb983bc2d9a986a6231de5f499
Josue Carrera <josuecarrera788@gmail.com>
2025-12-19 09:07:00 -0300

    agrego funcionalidad en home

--- frontend/src/app/component/mascota/mascotas-perdidas-page/mascotas-perdidas-page.ts
+++ frontend/src/app/component/mascota/mascotas-perdidas-page/mascotas-perdidas-page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { MisMascotas } from '../mis-mascotas/mis-mascotas';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
     selector: 'app-mascotas-perdidas-page',
     standalone: true,
-    imports: [MisMascotas],
+    imports: [MisMascotas, RouterModule,],
     template: `
-    <app-mis-mascotas 
+    <app-mis-mascotas
       [modo]="'perdidas'"
       [mostrarAcciones]="false"
       [titulo]="'Mascotas Perdidas'">
